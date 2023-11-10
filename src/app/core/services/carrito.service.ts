b3581a0ead33e16391ad78686f35ec71e208efe2
BautiCuria10 <136099092+BautiCuria10@users.noreply.github.com>
2023-11-10 10:19:58 -0300

    Add files via upload

--- src/app/core/services/carrito.service.ts
+++ src/app/core/services/carrito.service.ts
@@ -19,14 +19,27 @@ export class CarritoService {
   totalCarrito: number = 0;
 
   agregarProducto(producto:Producto, cantidad:number){
-    this.carrito.push({
-      producto: producto,
-      cantidad: cantidad
-    });
+    const index = this.carrito.findIndex(item => item.producto.id === producto.id);
+    if(index > -1) {
+      this.carrito[index].cantidad++; 
+    }
+    else {
+      this.carrito.push({
+        producto: producto,
+        cantidad: cantidad
+      });
+    }
     this.actualizarLocalstorage();
     this.calcularTotal();
   }
-
+  modificarCantidadProducto(idProducto:number, cantidadACambiar: number){
+    const index = this.carrito.findIndex(item => item.producto.id === idProducto);
+    if(index > -1) {
+      this.carrito[index].cantidad = cantidadACambiar; 
+    }
+    this.actualizarLocalstorage(); 
+    this.calcularTotal();
+  }
   eliminarProducto(id:number){
     this.carrito = this.carrito.filter(item => item.producto.id !== id);
     this.actualizarLocalstorage(); 
